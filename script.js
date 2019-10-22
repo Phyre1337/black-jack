f11b24b5428ca28993e4d0e46bac1512fb6aa412
Christian Torres <Torres.Christian@protonmail.com>
2019-10-22 05:39:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let newImg = null; // Defining the variable to display img
 let playerScore = 0; // Starts off player and dealer score at 0
 let dealerScore = 0;
 let hasAce = false;
+let dealerHiddenCard = null;
 
 // Defines object for the deck of cards
 let cards = {
@@ -171,77 +172,144 @@ function cardPicker(temp)
 };
 
 // Function to randomly pick card.
-function random()
+function random(spot)
 {
-    suit = cardPicker(Object.keys(cards)); // Makes array from properties of the "cards" object and pick random property
-    faces = Object.keys(cards[suit]); // Gives array from sub-object of "cards"
-    face = cardPicker(faces); // Randomly picks property of sub-object
-
-    suitLet = suit.charAt(0); // Stores first letter of suit.
-
-    // Try statement to stop repeats from happening.
-    try
+    if (spot == "player")
     {
-        faceLet = face.charAt(0); // Stores first letter of face of card.
+        suit = cardPicker(Object.keys(cards)); // Makes array from properties of the "cards" object and pick random property
+        faces = Object.keys(cards[suit]); // Gives array from sub-object of "cards"
+        face = cardPicker(faces); // Randomly picks property of sub-object
+
+        suitLet = suit.charAt(0); // Stores first letter of suit.
+
+        // Try statement to stop repeats from happening.
+        try
+        {
+            faceLet = face.charAt(0); // Stores first letter of face of card.
+        }
+        catch (ReferenceError) // Catches ReferenceError and restarts random function
+        {
+            random(player) // Restarts function
+        }
+        cardValue = cards[suit][face]; // Gets value of face property to determine the value of the card, used for the scoring system
+
+        newImg = document.createElement("img"); // Creates img element to display card
+        newImg.className = "card"; // Gives card class to img to specify styling
+
+        // If... else statement to determine the img file name, format used is [First letter or number of face][First letter of suit].png
+        if (face == "Ace") // If card is an Ace or jack...
+        {
+            newImg.src = `Images/${faceLet}${suitLet}.png`; // Sets src of img element to img name
+            document.getElementById("userCards").appendChild(newImg); // Appends img element to div on webpage
+            delete cards[suit][face]; // Deletes this card from the deck (cards object)
+            hasAce = true; // If the player has an ace, store this in a variable as true
+        } 
+        else if (face == "Jack")
+        {
+            newImg.src = `Images/${faceLet}${suitLet}.png`; // Sets src of img element to img name
+            document.getElementById("userCards").appendChild(newImg); // Appends img element to div on webpage
+            delete cards[suit][face]; // Deletes this card from the deck (cards object)
+        }
+        else if (face == "Queen" || face == "King") // Same for ones below
+        {
+            newImg.src = `Images/${faceLet}${suitLet}.png`;
+            document.getElementById("userCards").appendChild(newImg);
+            delete cards[suit][face];
+        }
+        else
+        {
+            newImg.src = `Images/${cardValue}${suitLet}.png`;
+            document.getElementById("userCards").appendChild(newImg);
+            delete cards[suit][face];
+        }
+
+        playerScore += cardValue // Adds card value to player score
+
+        if (hasAce == true && playerScore > 21) // If the player has an ace in their hand, and their score is over 21, change the ace to 1 instead of 11.
+        {
+            playerScore -= 10; // Subtracts 10 points from player score to change the ace from an 11 to a 1.
+            document.getElementById("score").innerHTML = playerScore; // Re-displays the player score
+            hasAce = false; // Changes it to false so this function doesn't run again when not supposed to
+        }
+
+        if (playerScore > 21) // If the player's score is over 21, the player loses.
+        {
+            document.getElementById("dealBtn").disabled = true; // Disables the hit button.
+            document.getElementById("score").innerHTML = "Busted." // Shows that the player has busted
+        }
+        else if (playerScore == 21) // If the player's score is 21, they win the game.
+        {
+            document.getElementById("dealBtn").disabled = true; // Disables the hit button 
+            document.getElementById("score").innerHTML = "21!!" // Displays winning message
+        }
+        else // If the player is still under 21, display player score and just continue
+        {
+            document.getElementById("score").innerHTML = playerScore; // Displays player score.
+        }
     }
-    catch (ReferenceError) // Catches ReferenceError and restarts random function
+    else if (spot == "dealerHidden")
     {
-        random() // Restarts function
-    }
-    cardValue = cards[suit][face]; // Gets value of face property to determine the value of the card, used for the scoring system
+        suit = cardPicker(Object.keys(cards)); // Makes array from properties of the "cards" object and pick random property
+        faces = Object.keys(cards[suit]); // Gives array from sub-object of "cards"
+        face = cardPicker(faces); // Randomly picks property of sub-object
 
-    newImg = document.createElement("img"); // Creates img element to display card
-    newImg.className = "card"; // Gives card class to img to specify styling
+        suitLet = suit.charAt(0); // Stores first letter of suit.
 
-    // If... else statement to determine the img file name, format used is [First letter or number of face][First letter of suit].png
-    if (face == "Ace") // If card is an Ace or jack...
-    {
-        newImg.src = `Images/${faceLet}${suitLet}.png`; // Sets src of img element to img name
-        document.getElementById("userCards").appendChild(newImg); // Appends img element to div on webpage
-        delete cards[suit][face]; // Deletes this card from the deck (cards object)
-        hasAce = true; // If the player has an ace, store this in a variable as true
-    } 
-    else if (face == "Jack")
-    {
-        newImg.src = `Images/${faceLet}${suitLet}.png`; // Sets src of img element to img name
-        document.getElementById("userCards").appendChild(newImg); // Appends img element to div on webpage
-        delete cards[suit][face]; // Deletes this card from the deck (cards object)
+        // Try statement to stop repeats from happening.
+        try
+        {
+            faceLet = face.charAt(0); // Stores first letter of face of card.
+        }
+        catch (ReferenceError) // Catches ReferenceError and restarts random function
+        {
+            random(dealerHidden) // Restarts function
+        }
+
+        cardValue = cards[suit][face]; // Gets value of face property to determine the value of the card, used for the scoring system
+
+        if (face == "Ace") // If card is an Ace or jack...
+        {
+            dealerHiddenCard = `Images/${faceLet}${suitLet}.png`
+            delete cards[suit][face];
+        } 
+        else if (face == "Jack")
+        {
+            dealerHiddenCard = `Images/${faceLet}${suitLet}.png`
+            delete cards[suit][face];
+        }
+        else if (face == "Queen" || face == "King") // Same for ones below
+        {
+            dealerHiddenCard = `Images/${faceLet}${suitLet}.png`
+            delete cards[suit][face];
+        }
+        else
+        {
+            dealerHiddenCard = `Images/${cardValue}${suitLet}.png`
+            delete cards[suit][face];
+        }
     }
-    else if (face == "Queen" || face == "King") // Same for ones below
+    else if (spot == "dealer")
     {
-        newImg.src = `Images/${faceLet}${suitLet}.png`;
-        document.getElementById("userCards").appendChild(newImg);
-        delete cards[suit][face];
+
+    }
+};
+
+function turns(turn)
+{
+    if (turn == "start")
+    {
+        random(player)
+        random(player)
+
+        random(dealerHidden)
+        random(dealer)
+    }
+    else if (turn == "hit")
+    {
+        random(player)
     }
     else
     {
-        newImg.src = `Images/${cardValue}${suitLet}.png`;
-        document.getElementById("userCards").appendChild(newImg);
-        delete cards[suit][face];
+        random(dealer)
     }
-
-    playerScore += cardValue // Adds card value to player score
-
-    if (hasAce == true && playerScore > 21) // If the player has an ace in their hand, and their score is over 21, change the ace to 1 instead of 11.
-    {
-        playerScore -= 10; // Subtracts 10 points from player score to change the ace from an 11 to a 1.
-        document.getElementById("score").innerHTML = playerScore; // Re-displays the player score
-        hasAce = false; // Changes it to false so this function doesn't run again when not supposed to
-    }
-
-    if (playerScore > 21) // If the player's score is over 21, the player loses.
-    {
-        document.getElementById("dealBtn").disabled = true; // Disables the hit button.
-        document.getElementById("score").innerHTML = "Busted." // Shows that the player has busted
-    }
-    else if (playerScore == 21) // If the player's score is 21, they win the game.
-    {
-        document.getElementById("dealBtn").disabled = true; // Disables the hit button 
-        document.getElementById("score").innerHTML = "21!!" // Displays winning message
-    }
-    else // If the player is still under 21, display player score and just continue
-    {
-        document.getElementById("score").innerHTML = playerScore; // Displays player score.
-    }
-
-};
+}
